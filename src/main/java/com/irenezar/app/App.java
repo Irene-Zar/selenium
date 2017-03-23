df0e515e1b2edb50dbd6fa3e14e5f852324aba94
Irene Zaremba <irene-y@yandex.ru>
2017-03-23 21:37:23 +0200

    Initial commit

---
+++ src/main/java/com/irenezar/app/App.java
@@ -0,0 +1,13 @@
+package com.irenezar.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
