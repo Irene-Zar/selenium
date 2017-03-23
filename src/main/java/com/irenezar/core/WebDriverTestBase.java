32532b6eafbc424a29533304305d0c67eeaffdde
Irene Zaremba <irene-y@yandex.ru>
2017-03-24 00:28:18 +0200

    First Google search test added

---
+++ src/main/java/com/irenezar/core/WebDriverTestBase.java
@@ -0,0 +1,31 @@
+package com.irenezar.core;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by new_user on 3/23/2017.
+ */
+public abstract class WebDriverTestBase {
+
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setUp(){
+        System.setProperty("webdriver.gecko.driver", "D:\\KIT\\selenium\\src\\test\\resources\\geckodriver\\geckodriver.exe");
+        //Init browser
+        driver = new FirefoxDriver();
+        //max window
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/com/irenezar/GoogleSearchTest.java
@@ -0,0 +1,33 @@
+package com.irenezar;
+
+import com.irenezar.core.WebDriverTestBase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+
+import static org.testng.AssertJUnit.assertEquals;
+
+
+/**
+ * Created by new_user on 3/20/2017.
+ */
+public class GoogleSearchTest extends WebDriverTestBase {
+
+    @Test
+    public void searchTest() {
+        driver.get("https://www.google.com.ua");
+        //Finds an element by name
+        WebElement searchField = driver.findElement(By.name("q"));
+        // Sends "Selenium" text into an input field
+        searchField.sendKeys("Selenium");
+        searchField.submit();
+        //Finds first link with specified result
+        WebElement seleniumLink = driver.findElement(By.xpath(".//*[@id='rso']/div/div/div[1]/div/h3/a"));
+        //Verifies a result
+
+        assertEquals(seleniumLink.getText().contains("Selenium"), true);
+    }
+
+}
